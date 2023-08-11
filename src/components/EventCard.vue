9206a5c66cdd504d43e04f69cf63f14a4243bcd6
thanap151255 <thanap151255@gmail.com>
2023-08-12 00:48:51 +0700

    set up

---
+++ src/components/EventCard.vue
@@ -0,0 +1,42 @@
+<template>
+      <RouterLink class="text-gray-700 no-underline" :to="{ name: 'event-detail', params: { id: event?.id } }">
+        <div class="mb-4">
+            <div class="p-5 w-64 cursor-pointer border border-gray-800 transform transition-transform duration-200 hover:scale-101 hover:shadow-md">
+        <span>@{{ event?.time }} on {{ event?.date }}</span>
+        <h4>{{ event?.title }}</h4>
+      </div>
+    </div>
+  </RouterLink>
+  </template>
+  
+  <script setup lang="ts">
+  import type { EventItem } from '@/type'
+  import type { PropType } from 'vue';
+  const props = defineProps({
+    event: {
+        type: Object as PropType<EventItem>,
+            require: true
+    }
+})
+
+  </script>
+  
+  <style scoped>
+  .event-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+  }
+  
+  .event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  .event-link {
+    color: #2c3e50;
+    text-decoration: none;
+  }
+  </style>
---
+++ src/views/StudentListView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import EventCard from '../components/EventCard.vue'
+import EventCard2 from '../components/EventCard2.vue'
+import StudentCard from '../components/StudentCard.vue'
+import type { EventItem } from '@/type';
+import { ref, type Ref } from 'vue';
+import StudentService from '@/services/StudentService'
+
+const events: Ref<Array<EventItem>> = ref([])
+  StudentService.getEvent().then((response) => {
+    events.value = response.data
+  })
+  
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <main class="events">
+    <StudentCard v-for="event in events" :key="event.id" :event="event"></StudentCard>
+  </main>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.events2 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: right;
+}
+</style>
