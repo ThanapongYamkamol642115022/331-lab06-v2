aad203a2f4987a467c6974fe3d44c5c2c0d9a585
thanap151255 <thanap151255@gmail.com>
2023-08-12 01:17:59 +0700

    2.11

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ const { message } = storeToRefs(store)
 
 <template>
   <header class="max-h-screen leading-normal">
+    <h1>Tanapong Yamkamol 642115022</h1>
     <div id="flashMessage" v-if="message" class="animate-yellowfade">
       <h4 class="text-lg">{{ message }}</h4>
     </div>
